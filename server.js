0e77a7acc83565af5e6946edbec3b7b8ac3af30f
aminugarbakyw2 <aminugarbakyw2@gmail.com>
2025-08-18 23:36:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,3 +10,15 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+// Route na donations
+app.get("/donate", (req, res) => {
+  res.json({ message: "Donation endpoint ready 💰" });
+});
+
+// Route na users
+app.get("/users", (req, res) => {
+  res.json([
+    { id: 1, name: "Aminu" },
+    { id: 2, name: "Garba" }
+  ]);
+});
