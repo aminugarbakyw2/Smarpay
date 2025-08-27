f1ac9c98dd6399083b35dd790251d7e6e50c0aa0
aminugarbakyw2 <aminugarbakyw2@gmail.com>
2025-08-27 15:21:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,4 +20,13 @@ app.get("/users", (req, res) => {
     { id: 1, name: "Aminu" },
     { id: 2, name: "Garba" }
   ]);
+});// Route na donations
+app.get("/donations", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM donations");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  }
 });
